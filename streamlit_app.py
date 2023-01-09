e05e9dd165d3f87b349a89440407f0e85e4a961d
CicelyFlournoy <122310005+cicelyfsf@users.noreply.github.com>
2023-01-09 13:43:23 -0600

    Update streamlit_app.py
    changed subheader to text for the menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,12 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
+
+
 streamlit.header('Breakfast Menu')
 
-streamlit.subheader('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 
-streamlit.subheader('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 
-streamlit.subheader('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg')
