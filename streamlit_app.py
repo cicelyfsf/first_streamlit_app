5b774670e480efb5b4625ac0cfb33cb45f42f073
CicelyFlournoy <122310005+cicelyfsf@users.noreply.github.com>
2023-01-19 13:31:32 -0600

    Update streamlit_app.py
    changed text response

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ streamlit.dataframe(my_data_rows)
 
 #New Section to display fruityvice api response for challenge lab
 fruit_choice = streamlit.text_input('What fruit would you like add?','jackfruit')
-streamlit.write('The user entered ', fruit_choice)
+streamlit.write('Thanks for adding ', fruit_choice)
